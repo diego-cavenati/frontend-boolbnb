725b764fbc7b0a3567949adb90c3655b1e7d9c05
diego-cavenati <diegocavenati@gmail.com>
2023-02-05 10:42:28 +0100

    add router with brief pages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import router
+import { router } from './router';
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-createApp(App).mount('#app')
+// use router
+createApp(App).use(router).mount('#app');
