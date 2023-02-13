57ee807c3b0a75530a35d28d7b0003e0ff1aedca
diego-cavenati <diegocavenati@gmail.com>
2023-02-13 14:32:23 +0100

    bho

--- src/views/AdvancedSearchView.vue
+++ src/views/AdvancedSearchView.vue
@@ -1,24 +1,117 @@
 <script>
 import { store } from '../store';
+import axios from 'axios';
+
 export default {
     name: 'AdvancedSearchView',
-    data (){
+    data() {
         return {
             store,
+            date: null,
+            apartment: null,
+            latitude: null,
+            longitude: null,
+            loading: true,
+            store,
+            client: null,
+            apartments: [],
         }
-    }
+    },
+    methods: {
+        addMarker() {
+            const tt = window.tt;
+            var location = [2.323653, 48.873261];
+            // var location = [this.longitude, this.latitude];
+            var popupOffset = 25;
+
+            var marker = new tt.Marker().setLngLat(location).addTo(this.map);
+            var popup = new tt.Popup({ offset: popupOffset }).setHTML("Your address!");
+            marker.setPopup(popup).togglePopup();
+        },
+        getMap() {
+            const tt = window.tt;
+            this.map = tt.map({
+                key: 'h0FDAudCcFnS8TK5dT1mvgXYkqCGc1CW',
+                container: this.$refs.mapRef,
+                style: 'tomtom://vector/1/basic-main',
+                center: [2.323653, 48.873261],
+                // center: [this.longitude, this.latitude],
+                zoom: 7,
+            });
+            this.map.addControl(new tt.FullscreenControl());
+            this.map.addControl(new tt.NavigationControl());
+        },
+        callApi() {
+            axios.get(`http://127.0.0.1:8000/api/apartments?page=${this.currentPage}`)
+                .then(response => {
+                    this.apartments = response.data.results.data;
+                    this.pages = response.data.results.last_page;
+                    console.log(this.apartments);
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.error(error)
+                    this.error = error.message;
+                    this.loading = false;
+                })
+        }
+    },
+    mounted() {
+        // const url = 'http://127.0.0.1:8000/api/apartments/';
+        // console.log(url);
+        // axios.get(url)
+        //     .then(resp => {
+        //         if (resp.data.success) {
+        //             this.apartment = resp.data.results;
+        //             this.latitude = resp.data.results.latitude;
+        //             this.longitude = resp.data.results.longitude;
+        //             this.loading = false;
+        //             this.getMap();
+        //             this.addMarker();
+        //         } else {
+        //             // this.$router.push({ name: 'not-found' }); //
+        //         }
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+    },
+    created() {
+        this.callApi()
+    },
 }
 </script>
 
 <template>
-    <!-- <CardComponent v-for="apartment in apartments" :key="apartment.id" :apartment="apartment" /> -->
-    <ul>
-        <li v-for="result in store.results">
-            {{ result.address }}
-        </li>
-    </ul>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+                <!-- <div class="container">
+                    <div class="row"> -->
+                <CardComponent v-for="apartment in apartments" :key="apartment.id" :apartment="apartment" />
+                <!-- </div>
+                </div> -->
+                <ul>
+                    <li v-for="result in store.results">
+                        {{ result.address }}
+                    </li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <div id='map' ref="mapRef"></div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/scss/general.scss';
+@use '../assets/scss/partials/variables.scss' as *;
+
+
+#map {
+    height: 40vh;
+    width: 100%;
+}
 </style>
