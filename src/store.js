46441b032c695b99abbd309069e218c951310e08
diego-cavenati <diegocavenati@gmail.com>
2023-02-16 14:03:03 +0100

    fix bug and ad placeholder

--- src/store.js
+++ src/store.js
@@ -11,12 +11,13 @@ export const store = reactive({
     //toggle class per i servizi
     isActive: false,
 
+    // tomtom key
+    tomtom_key: 'FiLLCEGWt31cQ9ECIWAD6zYjczzeC6zn',
     //searchbar
     address: '',
     check_in: '',
     check_out: '',
     price: '',
-    guests: '',
     results: [],
     loading: true,
     datePicker: '',
@@ -82,7 +83,6 @@ export const store = reactive({
     // raccolta di ID di servizi da inviare al back
     services_back: [],
 
-
     parseDates(dates) {
         let firstDate = new Date(dates[0]);
         let secondDate = new Date(dates[1]);
@@ -97,5 +97,10 @@ export const store = reactive({
         });
 
         this.$store.dispatch('updateDates', { firstDate, secondDate });
+    },
+    mutations: {
+        updateAddress(state, address) {
+            state.address = address;
+        }
     }
 })
